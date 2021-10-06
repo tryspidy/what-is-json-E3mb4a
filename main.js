5af3775bd291f77d25cedf2232cdd3644ebef5d6
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 22:20:32 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+● It is JavaScript Object Notation (is a minimal, readable format 
+                                    for structuring data.)
+● It is used primarily to transmit data between a server 
+and web application, as an alternative to XML.
+● Basically, a lightweight version of XML
+● In Key: Value format
+● Key is always in double quotes and value if 
+  string its double quotes and if numbers no quotes
+● It is purely based on http protocol, - 
+  so it hits the link on the browser and see the results
